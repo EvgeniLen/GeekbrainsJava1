00d9796061040942099803bc0704bbb4bb1177c1
EvgeniyLen <evgenilen@mail.ru>
2021-10-10 19:31:52 +0400

    Lesson6HomeWork1

---
+++ Lesson6/src/main/java/ru/geekbrains/homework/Animal.java
@@ -0,0 +1,21 @@
+package ru.geekbrains.homework;
+
+public abstract class Animal {
+    public static int countAnimals;
+    protected String name;
+    protected int age;
+    protected String color;
+
+    public Animal(String name, int age, String color) {
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
+
+    public abstract void run(int distance);
+
+    public abstract void swim (int distance);
+
+
+}
+
